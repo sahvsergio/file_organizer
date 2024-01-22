85b33f99108b07070a8756631df22541cbe2fb4c
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-01-21 22:11:23 -0500

    added a more specific read me file

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# file_organizer
+
+# File Organizer
+
+## What it does
+
+
+
+![Logo](https://dev-to-uploads.s3.amazonaws.com/uploads/articles/th5xamgrr6se0x5ro4g6.png)
+
+The file organizer  is an automation script  that uses python3 with its libraries os, shutil and rich so that it reads the names of the files in the Downloads folder and moves it to the corresponding based on their file extension as a way of automating moving files to their corresponding file.
+
+Ubuntu 23.10 desktop  was used as it is a linux distro that I am quite familiar with.
+
+Python 3.11 was used because it is the programming language I am more familiar with and also because its flexibility and multiple libraries which allow for ease in the implementation of os automation tasks. 
+
+The python libraries used were:
+
+*OS for os operations such as reading a directory's content, changing directories, filepath operations.
+
+*Shutil was used for its different file moving utilities.
+
+*Rich was used in order to provide some additional styling and features to the cli output and progress making it easier on the eyes for the end user.
+
+Some features I plan to implement in the future are :
+
+*Adding a  way of prompting the user to provide the filepath that they want to organize.
+
+*Adding a summary of the total files moves by filetype and extension.
+
+*Adding tests
+
+
+
+
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file
+
+text_extensions = ['.txt', '.doc', '.docx', '.pdf', '.html', '.csv']
+
+
+# Image file extensions
+image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff']
+
+# Audio file extensions
+audio_extensions = ['.mp3', '.wav', '.flac', '.aac', '.ogg', '.wma']
+
+# Video file extensions
+video_extensions = ['.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv']
+
+## Installation
+
+
+![Logo](https://dev-to-uploads.s3.amazonaws.com/uploads/articles/th5xamgrr6se0x5ro4g6.png)
+
--- file_organizer.py
+++ file_organizer.py
@@ -1,59 +1,69 @@
 """
-This file organizes files from the downloads folder into separate folders in the system, based on file extensions
+This file organizes
+files from the downloads
+folder into separate folders in the system,
 """
 # !/usr/bin/python3
 # standard library imports
 import shutil
 import os
 
-
-#related third party mports 
+# related third party imports
 import rich
+from rich import print as rprint
+from rich.console import Console
 from rich.prompt import Prompt
+from rich.progress import track
+from rich.layout import Layout
+from dotenv import load_dotenv
 
-hello=Prompt.ask('Enter the file path')
 
 
 
+#identiify the
 __author__ = 'Sergio Andrés Herrera Velásquez'
+__email__='sergioandresherrera@gmail.com'
 
-
-
-
-
-
-
-#python3 / home/sahvsergio/Documents/projects/file_organizer/file_organizer.py
-#
+# load the environment variables
+load_dotenv()
 
 
 # Text file extensions
-text_extensions = ['.txt', '.doc', '.docx', '.pdf', '.html', '.csv']
-
-
+text_extensions = os.getenv('text_extensions')
 # Image file extensions
-image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff']
-
+image_extensions = os.getenv('image_extensions')
 # Audio file extensions
-audio_extensions = ['.mp3', '.wav', '.flac', '.aac', '.ogg', '.wma']
-
+audio_extensions = os.getenv('audio_extensions')
 # Video file extensions
-video_extensions = ['.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv']
+video_extensions = os.getenv('video_extensions')
+#Archive_extensions
 archive_extensions=  shutil.get_archive_formats()
+
+
+#console
+console=Console()
+
+text_files=[]
+image_files=[]
+audio_files=[]
 
 
 # home directory
 with os.scandir(os.path.join(os.path.expanduser('~'), 'Downloads')) as files:
 
     for entry in files:
-        #
+        #destination paths
         text_dir = os.path.join(os.path.expanduser('~'), 'Documents')
         image_dir = os.path.join(os.path.expanduser('~'), 'Pictures')
         audio_dir = os.path.join(os.path.expanduser('~'), 'Music')
         video_dir = os.path.join(os.path.expanduser('~'), 'Videos')
-
-        if not entry.name.startswith('.') and entry.is_file:
-            entry_name, entry_ext = os.path.splitext(entry.name)
+        
+        with console.status("[bold green]Fetching data...") as status:
+            
+            
+            if not entry.name.startswith('.') and entry.is_file:
+                entry_name, entry_ext = os.path.splitext(entry.name)
+                
             if entry_ext in text_extensions:
                 shutil.move(os.path.join(os.path.expanduser(
                     '~'), 'Downloads', entry.name), os.path.join(text_dir, entry.name))
@@ -68,3 +78,4 @@ with os.scandir(os.path.join(os.path.expanduser('~'), 'Downloads')) as files:
                     '~'), 'Downloads', entry.name), os.path.join(video_dir, entry.name))
             elif entry_ext in archive_extensions[0] or  archive_extensions[1] or archive_extensions[2] or archive_extensions[3] or archive_extensions[4]:
                 shutil.move(os.path.join(os.path.expanduser('~'), 'Downloads', entry.name), os.path.join(text_dir, entry.name))
+            console.log(f'[bold][red]Done!')
